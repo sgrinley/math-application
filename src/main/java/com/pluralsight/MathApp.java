af8fcf1d88e6ca752766fa679c3ba1bb4cda3c2c
sgrinley <shamar808@yahoo.com>
2026-04-09 18:47:21 -0700

    find: Distance between two points results

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -37,5 +37,16 @@ public class MathApp {
 
         System.out.printf("The square root of %.1f is %.2f%n", number, squareRoot);
 
+//      QUESTION 5: FIND DISTANCE BETWEEN 2 POINTS
+
+        int x1 = 5;
+        int y1 = 10;
+        int x2 = 85;
+        int y2 = 50;
+
+        double distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+        System.out.printf("The distance between the two points is %.2f%n", distance);
+
     }
 }
