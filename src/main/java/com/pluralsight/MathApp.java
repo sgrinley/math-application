f3c6871a8918ce5ccb62dee57369eafbf47eccf3
sgrinley <shamar808@yahoo.com>
2026-04-09 18:06:20 -0700

    print: Display results

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -3,7 +3,15 @@ package com.pluralsight;
 public class MathApp {
     public static void main(String[] args) {
 
+//      EXERCISE 3
+//      QUESTION 1: FIND THE HIGHEST SALARY
 
+        double bobSalary = 75000;
+        double garySalary = 82000;
+
+        double highestSalary = Math.max(bobSalary, garySalary);
+
+        System.out.println("The highest salary is " + "$" + highestSalary);
 
 
     }
